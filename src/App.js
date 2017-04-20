9e93825dbc83be986176d675eebd547834e04a43
Mbairamadji Gaou Eric <eric_mbairamadji@MBPdeMbiramadji.libertelivinglab>
2017-04-20 13:42:09 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
-import './App.css';
-import  Home              from './components/Home';
+import  Homepage  from './components/HomePage/Homepage.js';
 
 
 
 class App extends Component {
-   
+
   render() {
       return (
          <Router history={browserHistory}>
-              <Route path="/" component={Home} />
-              
+              <Route path="/" component={Homepage} />
          </Router>
     );
   }
