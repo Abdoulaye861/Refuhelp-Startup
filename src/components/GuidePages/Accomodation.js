35ded59e4d38429e9252b694d26167e3f491e40c
beenie <abdoulayedssou@gmail.com>
2017-04-24 12:26:30 +0200

    guide pages update

--- src/components/GuidePages/Accomodation.js
+++ src/components/GuidePages/Accomodation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './guide.css';
 
 
 
@@ -6,7 +7,25 @@ class Accomodation extends React.Component {
    render() {
          return (
          <div>
-             <h3> Accomodation </h3>
+              <div className="titling">
+                <h3> Accomodation </h3>
+             </div>
+             <div className="guideMain">
+                <div className="guides">
+                   <div className="whereto"> <h3>Ou Manger</h3> </div>
+                   <div className="bouton"> <h4>Comment Y aller !</h4></div>
+                </div>
+
+                <div className="guides">
+                   <div className="whereto"> <h3>Ou Dormir</h3> </div>
+                   <div className="bouton"><h4>Comment Y aller !</h4></div>
+                </div>
+
+                <div className="guides">
+                   <div className="whereto"> <h3>Ou S'habiller</h3> </div>
+                   <div className="bouton"><h4>Comment Y aller !</h4></div>
+                </div>
+             </div>
          </div>
       );
     }
