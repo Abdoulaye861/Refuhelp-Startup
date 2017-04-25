6e4161b13fd8df7a2a7b06e30c960e555c4f2961
MBAIRAMADJI <mbairamadjieric@gmail.com>
2017-04-26 01:01:12 +0200

    main pages responsive

--- src/components/MainPage/Footer.js
+++ src/components/MainPage/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Faceicon from "../../img/facebook-icon.png";
+import Twittter from "../../img/twitter-icon.png";
 
 class Footer extends React.Component {
    render() {
          return (
            <div className="foot">
              <p> &copy; refuhelp 2017 </p>
+             <div className="social-networks">
+                   <a href="https://www.facebook.com/refuhelp.startup?fref=ts"><img className="face" src= {Faceicon}/></a>
+                   <a href="https://twitter.com/refu_help"><img className="twit" src={Twittter}/></a>
+             </div>            
            </div>
       );
     }
--- src/components/MainPage/MainHeader.js
+++ src/components/MainPage/MainHeader.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router";
 import Logo from "../../img/logo.png";
-import Faceicon from "../../img/facebook-icon.png";
-import Twittter from "../../img/twitter-icon.png";
+// import Faceicon from "../../img/facebook-icon.png";
+// import Twittter from "../../img/twitter-icon.png";
 import home from "../../img/home.png";
 import './main.css'
 
@@ -10,16 +10,8 @@ class MainHeader extends React.Component {
    render() {
          return (
          <div className="mainheader">
-            <div className="logo">
-                <Link to="/"><img src={Logo}/> </Link>
-
-            </div>
-            <Link to="main"><img className="home" src={home} alt="home"/></Link>
-             <div>
-               <div className="donate"> Faire un Don </div>
-               <a href="https://www.facebook.com/refuhelp.startup?fref=ts"><img className="face" src= {Faceicon}/></a>
-               <a href="https://twitter.com/refu_help"><img className="twit" src={Twittter}/></a>
-            </div>
+            <Link to="main"><img className="logo" src={Logo}/> </Link>
+            <div className="donate"> Faire un Don </div>
          </div>
       );
     }
