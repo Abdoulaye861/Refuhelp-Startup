d1c0b3041d5946c38dc9407465dbb88a2381c664
MBAIRAMADJI <mbairamadjieric@gmail.com>
2017-05-09 12:30:19 +0200

    no scroll

--- src/components/MainPage/Footer.js
+++ src/components/MainPage/Footer.js
@@ -5,12 +5,12 @@ import Twittter from "../../img/twitter-icon.png";
 class Footer extends React.Component {
    render() {
          return (
-           <div className="foot">  
+           <div className="foot">
              <div className="social-networks">
                    <a href="https://www.facebook.com/refuhelp.startup?fref=ts"><img className="face" src={Faceicon} alt="facebook"/></a>
                    <a href="https://twitter.com/refu_help"><img className="twit" src={Twittter} alt="twitter"/></a>
              </div>
-             <p> &copy; refuhelp 2017 </p>
+             {/* <p> &copy; refuhelp 2017 </p> */}
            </div>
       );
     }
