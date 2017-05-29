d3831da528cef4db6a6185badf732aef2a3a8136
beenie <abdoulayedssou@gmail.com>
2017-05-29 15:40:07 +0200

    buttons and titles update

--- src/components/GuidePages/Accomodation/Accomodation.js
+++ src/components/GuidePages/Accomodation/Accomodation.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Button, Popover, OverlayTrigger } from "react-bootstrap";
 import { Link } from 'react-router';
 import RefugeesModal from "./RefugeesModal";
-
 import './guide.css';
-
+import jaga from "../../../../src/img/ress.png";
+import tord from "../../../../src/img/hooo.png";
 class Accomodation extends React.Component {
   constructor(props){
     super(props);
@@ -30,7 +30,6 @@ class Accomodation extends React.Component {
                 <h3> Commodités </h3>
               </div>
 
-
                   <div className="cards">
                    <div className="guides  animated flipInX">
                       <div className="whereto"> <h3>Ou Dormir?</h3> </div>
@@ -38,11 +37,11 @@ class Accomodation extends React.Component {
                         <ul className="housing">
                           <li>En cas d'urgence, Contactez le Samu Social        <i className="fa fa-phone-square fa-2x" aria-hidden="true"></i><OverlayTrigger overlay={popover}><span> 115</span></OverlayTrigger></li>
 
-                          <li><Button onClick={() => this.setState({ showRefugeesModal: true })}>Se Loger! (video)</Button></li>
+                          <li> <img src={tord} /><Button onClick={() => this.setState({ showRefugeesModal: true })}>Se Loger! (video)</Button></li>
 
                             <RefugeesModal show={this.state.showRefugeesModal} onHide={refClose}/>
 
-                          <li><Link to="/accueil-du-jour"><Button>Accueil du jour</Button></Link></li>
+                          <li> <img src={tord} /><Link to="/accueil-du-jour"><Button>Accueil du jour</Button></Link></li>
 
                         </ul>
                       {/* <div className="bouton" onClick={this.open}><h5>Comment ça marche !</h5>
@@ -52,12 +51,13 @@ class Accomodation extends React.Component {
                        <div className="whereto"> <h3>Ou Manger?</h3> </div>
                        <h4 style={{textAlign:'center'}}>Repas Gratuits</h4>
                        <ul className="housing eating">
-                         <li><Button>Les Restos du coeur</Button></li>
+
+                         <li>  <img src={jaga} /><Button> Les Restos du coeur</Button> </li>
 
 
-                         <li><Button onClick={() => this.setState({ showAsylumModal: true })}>Epicéries sociales</Button></li>
+                         <li>  <img src={jaga} /> <Button onClick={() => this.setState({ showAsylumModal: true })}>Epicéries sociales</Button></li>
 
-                         <li><Button onClick={() => this.setState({ showRefugeesModal: true })}> Restaurants solidaires</Button></li>
+                         <li> <img src={jaga} /> <Button onClick={() => this.setState({ showRefugeesModal: true })}> Restaurants solidaires</Button></li>
 
                            <RefugeesModal show={this.state.showRefugeesModal} onHide={refClose}/>
                        </ul>
